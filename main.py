d4bf811f7a02263dd40ffd3da035d6a4acb68583
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2023-01-20 23:38:15 -0500

    dependencies

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
-print("hello")
+print(flask.__version__)
 
 # response = openai.Completion.create(
 #   model="text-davinci-003",
