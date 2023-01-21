a08dc9ac6b42bbba78340ad1c860031ae3067f46
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2023-01-20 23:57:54 -0500

    added basic html and css for flask web

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
-print(flask.__version__)
 
 # response = openai.Completion.create(
 #   model="text-davinci-003",
