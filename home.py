fa37be1eff28aae8eb070669ae4ab5128488ba1f
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2023-01-21 22:37:20 -0500

    can enter word to display corresponding color as background color

--- home.py
+++ home.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from main import openai_mood
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route('/', methods = ["GET", "POST"])
+# def hello():
+    # return render_template('index.html')
+def getMood():
+    response = "white"
+    if request.method == 'POST':
+        mood = request.form.get("mood")
+
+        myPrompt = "The CSS code for a color of " + mood + ":\n\nbackground-color:"
+        response = openai_mood(myPrompt)
+        print(response)
+
+        # take response and make it into background color
+
+    return render_template("index.html", mood=response)
+
+    
+
+if __name__ == '__main__':
+    app.run()
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
 import openai
-import flask
 from dotenv import load_dotenv
 
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# response = openai.Completion.create(
-#   model="text-davinci-003",
-#   prompt="The CSS code for a color of sad, depression, and dogs:\n\nbackground-color: #6A5ACD",
-#   temperature=0,
-#   max_tokens=256,
-#   top_p=1,
-#   frequency_penalty=0,
-#   presence_penalty=0,
-#   stop=[";"]
-# )
+def openai_mood(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=[";"]
+    )
+    message = response.choices[0].text
+    return message.strip()
